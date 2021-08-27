ccc398a80ba0a851ab190a62ba1c37863e25c937
Alexander Lopatin <alopatindev@gmail.com>
2021-08-27 20:22:23 +0300

    Initial

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use anyhow::Context;
+use anyhow::Result;
+use clap::App;
+use nvim_rs::{
+    create::tokio::{new_path, new_tcp},
+    rpc::handler::Dummy,
+};
+use tokio::net::lookup_host;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let matches = App::new("nvim-send")
+        .args_from_usage(
+            "--remote-send=[keys]     'Send key presses'
+             --servername=[address]   'Set the address to be used'",
+        )
+        .get_matches();
+
+    let server_address = matches
+        .value_of("servername")
+        .context("servername not found")?;
+
+    let keys = matches.value_of("remote-send").context("keys not found")?;
+
+    if lookup_host(server_address).await.is_ok() {
+        let handler = Dummy::new();
+        let (neovim, _job_handler) = new_tcp(server_address, handler).await?;
+        dbg!(&neovim.input(keys).await?);
+    } else {
+        let handler = Dummy::new();
+        let (neovim, _job_handler) = new_path(server_address, handler).await?;
+        dbg!(&neovim.input(keys).await?);
+    }
+
+    Ok(())
+}
