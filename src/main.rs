e98674f5b6a156eae7096d128c8cd99b9ff34ce5
Alexander Lopatin <alopatindev@gmail.com>
2021-08-28 18:51:35 +0300

    Some progress

--- src/main.rs
+++ src/main.rs
@@ -22,15 +22,16 @@ async fn main() -> Result<()> {
 
     let keys = matches.value_of("remote-send").context("keys not found")?;
 
-    if lookup_host(server_address).await.is_ok() {
+    let bytes_written = if lookup_host(server_address).await.is_ok() {
         let handler = Dummy::new();
         let (neovim, _job_handler) = new_tcp(server_address, handler).await?;
-        dbg!(&neovim.input(keys).await?);
+        neovim.input(keys).await?
     } else {
         let handler = Dummy::new();
         let (neovim, _job_handler) = new_path(server_address, handler).await?;
-        dbg!(&neovim.input(keys).await?);
-    }
+        neovim.input(keys).await?
+    };
+    debug_assert_eq!(bytes_written as usize, keys.as_bytes().len());
 
     Ok(())
 }
