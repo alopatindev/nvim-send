4fe4005fe0517237d6548a3da0ee43fb6599d877
Alexander Lopatin <alopatindev@gmail.com>
2022-10-23 03:16:30 +0800

    Show error message if connection has failed, process both --remote-send and --command at the same time in any order

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use anyhow::Context;
-use anyhow::Result;
-use clap::{crate_authors, crate_description, crate_name, crate_version, App};
+use anyhow::{format_err, Context, Result};
+use clap::{arg, builder::Command, crate_authors, crate_description, crate_name, crate_version};
 use nvim_rs::{
     create::tokio::{new_path, new_tcp},
     rpc::handler::Dummy,
@@ -8,40 +7,54 @@ use nvim_rs::{
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let matches = App::new(crate_name!())
+    let matches = Command::new(crate_name!())
         .version(crate_version!())
         .about(crate_description!())
         .author(crate_authors!("\n"))
-        .args_from_usage(
-            "--command=[command]      'Execute command'
-             --remote-send=[keys]     'Send key presses'
-             --servername=[address]   'Set the address to be used'",
-        )
+        .arg(arg!(--command <command> "Execute command").required(false))
+        .arg(arg!(--"remote-send" <keys> "Send key presses").required(false))
+        .arg(arg!(--servername <address> "Set the address to be used"))
         .get_matches();
 
     let server_address = matches
-        .value_of("servername")
+        .get_one::<String>("servername")
         .context("servername not found")?;
 
     let handler = Dummy::new();
     if let Ok((neovim, _job_handler)) = new_tcp(server_address, handler).await {
-        if let Some(command) = matches.value_of("command") {
-            neovim.command(command).await?;
+        let args = matches
+            .ids()
+            .map(|i| i.as_str())
+            .filter(|i| i != &"servername");
+        for i in args {
+            if let Some(value) = matches.get_one::<String>(i) {
+                if i == "command" {
+                    neovim.command(value).await?;
+                } else if i == "remote-send" {
+                    neovim.input(value).await?;
+                }
+            }
         }
-        if let Some(keys) = matches.value_of("remote-send") {
-            neovim.input(keys).await?;
-        }
+        return Ok(());
     } else {
         let handler = Dummy::new();
         if let Ok((neovim, _job_handler)) = new_path(server_address, handler).await {
-            if let Some(command) = matches.value_of("command") {
-                neovim.command(command).await?;
+            let args = matches
+                .ids()
+                .map(|i| i.as_str())
+                .filter(|i| i != &"servername");
+            for i in args {
+                if let Some(value) = matches.get_one::<String>(i) {
+                    if i == "command" {
+                        neovim.command(value).await?;
+                    } else if i == "remote-send" {
+                        neovim.input(value).await?;
+                    }
+                }
             }
-            if let Some(keys) = matches.value_of("remote-send") {
-                neovim.input(keys).await?;
-            }
+            return Ok(());
         }
     }
 
-    Ok(())
+    Err(format_err!("cannot connect"))
 }
